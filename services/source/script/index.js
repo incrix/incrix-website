75868100430ed6dd2e96f798e5ec0f2c12905248
AvinashSethu <avinash97official@gmail.com>
2022-05-16 11:50:49 +0530

    Added services page

---
+++ services/source/script/index.js
@@ -0,0 +1,24 @@
+const menu = document.querySelector('.ham');
+const nav = document.querySelector('.nav');
+let menuOpen = false;
+
+menu.addEventListener('click', ()=>{
+    if(!menuOpen){
+        menu.classList.add('open');
+        menuOpen=true;
+        nav.style.right='0px';
+    }
+    else{
+        menu.classList.remove('open');
+        menuOpen=false;  
+        nav.style.right='-1000px';
+    }
+});
+
+
+
+jQuery(document).ready(function() {
+    jQuery('.backg').fadeOut(2500);
+});
+
+
